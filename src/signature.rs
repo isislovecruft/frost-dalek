ff7f3f730605d0421509eadc031b99986f02a9f1
Isis Lovecruft <isis@patternsinthevoid.net>
2020-09-29 23:47:15 +0000

    Begin implementing signature aggregation.

--- src/signature.rs
+++ src/signature.rs
@@ -9,6 +9,17 @@
 
 //! FROST signatures and their creation.
 
+#[cfg(feature = "alloc")]
+use alloc::collections::HashMap;
+#[cfg(feature = "alloc")]
+use alloc::collections::hash_map::Values;
+
+#[cfg(feature = "std")]
+use std::collections::HashMap;
+#[cfg(feature = "std")]
+use std::collections::hash_map::Values;
+
+use curve25519_dalek::constants::RISTRETTO_BASEPOINT_TABLE;
 use curve25519_dalek::ristretto::RistrettoPoint;
 use curve25519_dalek::scalar::Scalar;
 use curve25519_dalek::traits::Identity;
@@ -16,13 +27,19 @@ use curve25519_dalek::traits::Identity;
 use sha2::Digest;
 use sha2::Sha512;
 
+use crate::keygen::GroupKey;
+use crate::keygen::PublicShare as IndividualPublicKey;
 use crate::keygen::SecretKey;
+use crate::parameters::Parameters;
 use crate::precomputation::CommitmentShare;
 
 // assume central aggregator does coordination
 
 // nonces should be explicitly drop()ed from memory (and probably even zeroed
 // first)
+
+// XXX Nonce reuse is catastrophic and results in obtaining an individual
+//     signer's long-term secret key; it must be prevented at all costs.
 
 /// An individual signer in the threshold signature scheme.
 // XXX need sorting method
@@ -45,13 +62,50 @@ pub struct PartialThresholdSignature {
 /// A complete, aggregated threshold signature.
 pub struct ThresholdSignature(pub(crate) Scalar, pub(crate) Scalar);
 
+// XXX I hate this so much.
+//
+// XXX TODO there might be a more efficient way to optimise this data structure
+//     and its algorithms?
+struct SignerRs (pub(crate) HashMap<u32, RistrettoPoint>);
+
+impl SignerRs {
+    pub(crate) fn new() -> Self {
+        SignerRs(Hashmap::new())
+    }
+
+    // XXX [CFRG] Since the sorting order matters for the public API, both it
+    // and the canonicalisation of the participant indices needs to be
+    // specified.
+    pub(crate) fn insert(&mut self, index: &u32, point: &RistrettoPoint) {
+        self.0.insert(index.to_be_bytes(), point);
+    }
+
+    pub(crate) fn get(&self, index: &u32) -> Option<RistrettoPoint> {
+        self.0.get(index.to_be_bytes())
+    }
+
+    pub(crate) fn sorted(&self) -> Vec<(u32, RistrettoPoint)> {
+        let mut sorted: Vec<(u32, RistrettoPoint)> = Vec::with_capacity(self.0.len());
+
+        for (i, point) in self.0.iter() {
+            let index = u32::from_be_bytes(&i);
+            sorted.insert(index, (index, point));
+        }
+        sorted
+    }
+
+    pub(crate) fn values(&self) -> Values<'_, u32, RistrettoPoint> {
+        self.0.values()
+    }
+}
+
 fn compute_binding_factors_and_group_commitment(
     message: &[u8],
     signers: &Vec<Signer>,
-) -> (Vec<Scalar>, RistrettoPoint)
+) -> (Vec<Scalar>, SignerRs)
 {
 	let mut binding_factors: Vec<Scalar> = Vec::with_capacity(signers.len());
-    let mut R: RistrettoPoint = RistrettoPoint::identity();
+    let mut Rs: SignerRs = SignerRs::new(); // XXX can we optimise size?
 
     for signer in signers.iter() {
         let hiding = signer.published_commitment_share.0;
@@ -73,12 +127,21 @@ fn compute_binding_factors_and_group_commitment(
         let binding_factor = Scalar::from_hash(h1);
 
         // THIS IS THE MAGIC STUFF ↓↓↓
-        R += hiding + (binding_factor * binding);
+        Rs.insert(signer.participant_index, hiding + (binding_factor * binding));
 
 	    binding_factors.push(binding_factor);
     }
 
-    (binding_factors, R)
+    (binding_factors, Rs)
+}
+
+fn compute_challenge(message: &[u8], R: &RistrettoPoint) -> Scalar {
+    let mut h2 = Sha512::new();
+
+    h2.update(message);
+    h2.update(R.compress().as_bytes());
+
+    Scalar::from_hash(h2)
 }
 
 /// Compute an individual signer's [`PartialThresholdSignature`] contribution to
@@ -103,14 +166,9 @@ pub fn sign(
     signers: &Vec<Signer>,
 ) -> PartialThresholdSignature
 {
-    let (binding_factors, R) = compute_binding_factors_and_group_commitment(&message, &signers);
-
-    let mut h2 = Sha512::new();
-
-    h2.update(message);
-    h2.update(R.compress().as_bytes());
-
-    let challenge = Scalar::from_hash(h2);
+    let (binding_factors, Rs) = compute_binding_factors_and_group_commitment(&message, &signers);
+    let R = Rs.iter().map(|x| x.1).collect().sum();
+    let challenge = compute_challenge(&message, &R);
 
     // XXX We can't use the participant index to index into the binding factors
     // here because the list of actual signers might be different than the total
@@ -142,4 +200,89 @@ pub fn sign(
     // XXX ... I.... don't really love this API?
 
     PartialThresholdSignature { index: my_secret_key.index, z }
+}
+
+/// A signature aggregator is an untrusted party who coalesces all of the
+/// participating signers' published commitment shares and their
+/// [`PartialThresholdSignature`] and creates the final [`ThresholdSignature`].
+/// The signature aggregator may even be one of the `t` participants in this
+/// signing operation.
+pub struct SignatureAggregator<'sa> {
+    /// The protocol instance parameters.
+    pub(crate) parameters: Parameters,
+    /// The set of signing participants for this round.
+    pub(crate) signers: Vec<Signer>,
+    /// The signer's public keys for verifying their [`PartialThresholdSignature`].
+    pub(crate) public_keys: Vec<IndividualPublicKey>,
+    /// The message to be signed.
+    pub(crate) message: &'sa [u8],
+}
+
+impl SignatureAggregator {
+    /// Construct a new signature aggregator from some protocol instantiation
+    /// `parameters` and a `message` to be signed.
+    pub fn new<'sa>(parameters: Parameters, message: &'sa [u8]) -> SignatureAggregator<'sa> {
+        // XXX Can t here be some t' s.t. t ≤ t' ≤ n from the parameters?
+        let signers: Vec<Signer> = Vec::with_capacity(parameters.t as usize);
+        let public_keys: Vec<IndividualPublicKey> = Vec::with_capacity(parameters.t as usize);
+
+        SignatureAggregator { parameters, signers, public_keys, message }
+    }
+
+    /// Include a signer in the protocol.
+    pub fn include_signer(&mut self, signer: Signer, public_key: IndividualPublicKey) {
+        self.signers.push(signer);
+        self.public_keys.push(public_key);
+    }
+
+    /// Aggregate a set of partial signatures
+    pub fn aggregate(
+        &mut self,
+        partial_signatures: &Vec<PartialThresholdSignature>
+    ) -> Result<ThresholdSignature, Vec<u32>>
+    {
+        //self.signers.sort(); // XXX need signers.sort() impl
+        
+        let (binding_factors, Rs) = compute_binding_factors_and_group_commitment(&self.message, &self.signers);
+        let R = Rs.values().sum();
+        let challenge = compute_challenge(&self.message, &R);
+        let lambda: Scalar = binding_factors.iter().sum();
+
+        let misbehaving_participants: Vec<u32> = Vec::new();
+
+        for signer in self.signers.iter() {
+            // XXX wrong index, impl on hashmap
+            let check = &RISTRETTO_BASEPOINT_TABLE * partial_signatures.get(signer.participant_index);
+            // XXX wrong index, impl on hashmap
+            let Y_i = public_keys.get(signer.participant_index);
+
+            match Rs.get(signer.participant_index) {
+                Some(R_i) => {
+                    match check == R_i + &(&Y_i * (challenge * lambda)) { // XXX lambda why???
+                        true  => continue,
+                        false => misbehaving_participants.push(signer.participant_index),
+                    }
+                },
+                // XXX [DIFFERENT_TO_PAPER] We're reporting missing signers
+                //     (possibly the fault of the aggregator) as well as
+                //     misbehaved participants.
+                None => misbehaving_participants.push(signer.participant_index),
+            }
+        }
+
+        unimplemented!()
+    }
+}
+
+impl ThresholdSignature {
+    /// Verify this [`ThresholdSignature`].
+    ///
+    /// # Returns
+    ///
+    /// A `Result` whose `Ok` value is an empty tuple if the threshold signature
+    /// was successfully verified, otherwise a vector of the participant indices
+    /// of any misbehaving participants.
+    pub fn verify(&self, group_key: &GroupKey) -> Result<(), Vec<u32>> {
+        unimplemented!()
+    }
 }
