d51b413492bcc36e9c8d781636fa777b52dee069
Isis Lovecruft <isis@patternsinthevoid.net>
2020-10-01 00:21:08 +0000

    Change signature aggregator to store the partial signatures.

--- src/signature.rs
+++ src/signature.rs
@@ -82,7 +82,10 @@ pub struct PartialThresholdSignature {
 }
 
 /// A complete, aggregated threshold signature.
-pub struct ThresholdSignature(pub(crate) Scalar, pub(crate) Scalar);
+pub struct ThresholdSignature {
+    pub(crate) z: Scalar,
+    pub(crate) c: Scalar,
+}
 
 macro_rules! impl_indexed_hashmap {
     (Type = $type:ident, Item = $item:ident) => {
@@ -131,7 +134,7 @@ impl_indexed_hashmap!(Type = SignerRs, Item = RistrettoPoint);
 
 /// A type for storing signers' partial threshold signatures along with the
 /// respective signer participant index.
-pub struct PartialThresholdSignatures(pub(crate) HashMap<[u8; 4], Scalar>);
+pub(crate) struct PartialThresholdSignatures(pub(crate) HashMap<[u8; 4], Scalar>);
 
 impl_indexed_hashmap!(Type = PartialThresholdSignatures, Item = Scalar);
 
@@ -216,6 +219,8 @@ pub fn sign(
     // here because the list of actual signers might be different than the total
     // number of possible participants.  This means we need to waste a few
     // cycles recomputing our own blinding factor. :(
+    //
+    // XXX FIXME Signers contains the participant index so we should return it from compute_binding..()
     let mut h3 = Sha512::new();
 
     // [DIFFERENT_TO_PAPER] I added a context string.
@@ -258,6 +263,9 @@ pub struct SignatureAggregator<'sa> {
     pub(crate) public_keys: IndividualPublicKeys,
     /// The message to be signed.
     pub(crate) message: &'sa [u8],
+    /// The partial signatures from individual participants which have been
+    /// collected thus far.
+    pub(crate) partial_signatures: PartialThresholdSignatures,
 }
 
 impl SignatureAggregator<'_> {
@@ -266,9 +274,10 @@ impl SignatureAggregator<'_> {
     pub fn new<'sa>(parameters: Parameters, message: &'sa [u8]) -> SignatureAggregator<'sa> {
         // XXX Can t here be some t' s.t. t ≤ t' ≤ n from the parameters?
         let signers: Vec<Signer> = Vec::with_capacity(parameters.t as usize);
-        let public_keys: IndividualPublicKeys = IndividualPublicKeys::new();
+        let public_keys = IndividualPublicKeys::new();
+        let partial_signatures = PartialThresholdSignatures::new();
 
-        SignatureAggregator { parameters, signers, public_keys, message }
+        SignatureAggregator { parameters, signers, public_keys, message, partial_signatures }
     }
 
     /// Include a signer in the protocol.
@@ -276,25 +285,45 @@ impl SignatureAggregator<'_> {
     /// # Panics
     ///
     /// If the `signer.participant_index` doesn't match the `public_key.index`.
-    pub fn include_signer(&mut self, signer: Signer, public_key: IndividualPublicKey) {
-        assert_eq!(signer.participant_index, public_key.index);
+    pub fn include_signer(
+        &mut self,
+        participant_index: u32,
+        published_commitment_share: (RistrettoPoint, RistrettoPoint),
+        public_key: IndividualPublicKey)
+    {
+        assert_eq!(participant_index, public_key.index,
+                   "Tried to add signer with participant index {}, but public key is for participant with index {}",
+                   participant_index, public_key.index);
 
-        self.signers.push(signer);
+        self.signers.push(Signer { participant_index, published_commitment_share });
         self.public_keys.insert(&public_key.index, public_key.share);
     }
 
+    /// Get the list of partipating signers.
+    ///
+    /// # Returns
+    ///
+    /// A `Vec<Signer>` of the participating signers in this round.
+    pub fn get_signers<'sa>(&'sa mut self) -> &'sa Vec<Signer> {
+        self.signers.dedup();
+        self.signers.sort();
+
+        &self.signers
+    }
+
+    /// Add a [`PartialThresholdSignature`] to be included in the aggregation.
+    pub fn include_partial_signature(&mut self, partial_signature: PartialThresholdSignature) {
+        self.partial_signatures.insert(&partial_signature.index, partial_signature.z);
+    }
+
     /// Aggregate a set of partial signatures
-    pub fn aggregate(
-        &mut self,
-        partial_signatures: &PartialThresholdSignatures,
-    ) -> Result<ThresholdSignature, Vec<u32>>
-    {
+    pub fn aggregate(&mut self) -> Result<ThresholdSignature, Vec<u32>> {
         self.signers.sort();
         self.signers.dedup();
 
         let mut misbehaving_participants: Vec<u32> = Vec::new();
         
-        if self.signers.len() != self.parameters.t {
+        if self.signers.len() != self.parameters.t as usize {
             return Err(misbehaving_participants);
         }
 
@@ -307,7 +336,7 @@ impl SignatureAggregator<'_> {
         for signer in self.signers.iter() {
             // XXX [DIFFERENT_TO_PAPER] We're reporting missing partial
             //     signatures which could possibly be the fault of the aggregator.
-            let partial_sig = match partial_signatures.get(&signer.participant_index) {
+            let partial_sig = match self.partial_signatures.get(&signer.participant_index) {
                 Some(x) => x,
                 None => {
                     misbehaving_participants.push(signer.participant_index);
@@ -342,7 +371,7 @@ impl SignatureAggregator<'_> {
 
         match misbehaving_participants.len() > 0 {
             true => Err(misbehaving_participants),
-            false => Ok(ThresholdSignature(z, c)),
+            false => Ok(ThresholdSignature {z, c}),
         }
     }
 }
